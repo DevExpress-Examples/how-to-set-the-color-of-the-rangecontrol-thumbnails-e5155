dfca5fd3206c5540a58062e2f6243fd56983db8b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to set the color of the RangeControl thumbnails
+
+
+<p>This example demonstrates how you can specify the color of the <a href="http://help.devexpress.com/#WPF/CustomDocument15026">RangeControl</a> thumbnails in code. <br /> The background color of the thumbnail is determined by the ThumbnailControl template. The Border.Background property of the thumbnail is bound to the appointment it represents and uses a custom AppointmentToColorConverter. The converter specifies the color according to words encountered in the Appointment.Location string.<br /><br /><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-set-the-color-of-the-rangecontrol-thumbnails-e5155/14.2.3+/media/253d6038-f3fb-11e4-80bf-00155d62480c.png"></p>
+
+<br/>
+
+
---
+++ VB/RangeControlColors/MainWindow.xaml.vb
@@ -0,0 +1,89 @@
+﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.XtraScheduler
+Imports System
+Imports System.Globalization
+Imports System.Windows.Data
+Imports System.Windows.Markup
+Imports System.Windows.Media
+
+Namespace RangeControlColors
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits DXWindow
+
+        Public Sub New()
+            InitializeComponent()
+            AddHandler scheduler.AppointmentViewInfoCustomizing, AddressOf scheduler_AppointmentViewInfoCustomizing
+            scheduler.Start = Date.Today
+            CreateAppointments()
+        End Sub
+
+        Private Sub scheduler_AppointmentViewInfoCustomizing(ByVal sender As Object, ByVal e As DevExpress.Xpf.Scheduler.AppointmentViewInfoCustomizingEventArgs)
+            If e.ViewInfo.Location = String.Empty Then
+                e.ViewInfo.Label = New DevExpress.Xpf.Scheduler.AppointmentLabel(Colors.White, "")
+            End If
+            If e.ViewInfo.Location.Contains("Orange") Then
+            e.ViewInfo.Label = New DevExpress.Xpf.Scheduler.AppointmentLabel(Colors.Orange, "")
+            End If
+            If e.ViewInfo.Location.Contains("Green") Then
+            e.ViewInfo.Label = New DevExpress.Xpf.Scheduler.AppointmentLabel(Colors.Green, "")
+            End If
+            If e.ViewInfo.Location.Contains("Blue") Then
+            e.ViewInfo.Label = New DevExpress.Xpf.Scheduler.AppointmentLabel(Colors.Blue, "")
+            End If
+        End Sub
+
+        Private Sub CreateAppointments()
+            Dim keyWords() As String = { "Orange", "Green", "Blue" }
+            For i As Integer = 0 To keyWords.Length - 1
+                Dim apt As New Appointment()
+                apt.Start = Date.Now.AddHours(i)
+                apt.End = Date.Now.AddHours(i + 1)
+                apt.Location += keyWords(i)
+                scheduler.Storage.AppointmentStorage.Add(apt)
+            Next i
+        End Sub
+    End Class
+    #Region "#AppointmentToColorConverter"
+    Public Class AppointmentToColorConverter
+        Inherits MarkupExtension
+        Implements IValueConverter
+
+        Private instance As AppointmentToColorConverter = Nothing
+
+        Public Sub New()
+        End Sub
+
+        Public Property DefaultColor() As Color
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            If instance Is Nothing Then
+                instance = New AppointmentToColorConverter()
+                instance.DefaultColor = DefaultColor
+            End If
+            Return instance
+        End Function
+        Private Function IValueConverter_Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            Dim apt As Appointment = TryCast(value, Appointment)
+            If apt.Location = String.Empty Then
+                Return Brushes.White
+            End If
+            If apt.Location.Contains("Orange") Then
+                Return Brushes.Orange
+            End If
+            If apt.Location.Contains("Green") Then
+                Return Brushes.Green
+            End If
+            If apt.Location.Contains("Blue") Then
+                Return Brushes.Blue
+            End If
+            Return Brushes.Gray
+        End Function
+        Private Function IValueConverter_ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Return DirectCast(value, SolidColorBrush).Color
+        End Function
+    End Class
+    #End Region ' #AppointmentToColorConverter
+End Namespace
